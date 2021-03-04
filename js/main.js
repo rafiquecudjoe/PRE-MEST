889f86991796432ec37bdf7772ff4eabca42dad0
Rafique <rasgalazy5@gmail.com>
2021-03-04 12:28:35 +0000

    worked on js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
 console.log("helllooooooooo")
+//variable
 
 let b = 78
 
 console.log(b)
+
+
+let myname = "Rafique Adam Cudjoe"
+
+console.log(myname)
+
+
+let g = 5
+let h = 10
+
+h =11
+
+console.log(g + h);
+ 
+console.log(g * h);
+
+
+let surname = "Rafique"
+
+let firstname = " Adam Cudjoe"
+
+console.log(surname + firstname);
+
+// Arrays and Object
+
+let myarray = [8, 4, 10, 13, 34, 54, 90]
+
+let mynames = ["Rafique","Adam" ,"Cudjoe"]
+
+
+console.log(mynames);
+
+console.log(myarray[3]);
+
+
+myarray.push(50)
+
+console.log(myarray);
+
+mynames.push("Flipmone")
+
+console.log(mynames);
